e363d18dbea683e5d15a8d604803664666d2b585
shlomijs <shlomi@tikalk.com>
2015-08-09 23:13:00 +0300

    add home controller

---
+++ client/app/home/home.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('changingStatusApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: 'app/home/home.html',
+        controller: 'HomeCtrl'
+      });
+  });
